10d6e92c724917d9f0e557860b5d8493017263d2
Trevor Jones <tjones009@yahoo.com>
2020-03-02 16:49:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,7 +15,7 @@ def directors_totals(nds)
   results = {}
   director_index = 0
   
-  while director_index < nds.lenght do
+  while director_index < nds.length do
     director = nds[director_index]
     result[director[:name]] = gross_for_director(director)
     director_index += 1
@@ -23,8 +23,3 @@ def directors_totals(nds)
   results
 end
 
-def gross_for_director(director_data)
-  total = 0
-  index = 0
-  
-  while index < director_data[:movies].length do
